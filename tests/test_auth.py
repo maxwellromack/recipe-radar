c4852ac332c2723241487bf1840e1e588294ccfc
Max <maxwellromack@gmail.com>
2023-05-25 16:29:35 -0400

    wrote login tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,7 @@
 import json
 import pytest
+from flask import g, session
+from backend.db import get_db
 
 def test_register(client):
     payload = {
@@ -28,4 +30,35 @@ def test_register_validate_input(client, username, password, message):
     response = client.post('/auth/register', data = json_payload, content_type = 'application/json')
 
     assert response.status_code == 400
-    assert response.get_json() == {'error': message}
+    recieved_message = json.loads(response.get_json())
+    assert message in recieved_message['error']
+
+def test_login(client):
+    payload = {
+        'username': 'test',
+        'password': 'test'
+    }
+
+    json_payload = json.dumps(payload)
+    response = client.post('auth/login', data = json_payload, content_type = 'application/json')
+
+    assert response.status_code == 200
+    assert session['user_id'] == 1
+    assert g.user['username'] == 'test'
+
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username'),
+    ('test', 'a', b'Incorrect password'),
+))
+def test_login_validate_input(client, username, password, message):
+    payload = {
+        'username': username,
+        'password': password
+    }
+
+    json_payload = json.dumps(payload)
+    response = client.post('/auth/login', data = json_payload, content_type = 'application/json')
+
+    assert response.status_code == 400
+    recieved_message = json.loads(response.get_json())
+    assert message in recieved_message['error']
