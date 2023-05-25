5c89a711c19ea26de746df6a5da631196083834f
Max <maxwellromack@gmail.com>
2023-05-25 09:47:07 -0400

    added register view

--- backend/auth.py
+++ backend/auth.py
@@ -6,3 +6,31 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from backend.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix = '/auth')
+
+@bp.route('/register', methods = ('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Password is required"
+
+        if error is None:
+            try:    # try to add the new user to the database
+                db.execute( 
+                    'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            except db.IntegrityError:   # username already exists
+                error = f"User {username} os already registered."
+            else:   # prompt user to login
+                return redirect(url_for('auth.login'))
+        flash(error)
+    
+    return render_template('auth/register.html')
