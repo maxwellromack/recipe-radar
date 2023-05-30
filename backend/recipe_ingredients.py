fd29038276ef72633e132dec9d403efa055ce94d
Max <78194977+maxwellromack@users.noreply.github.com>
2023-05-30 12:24:39 -0400

    Update recipe_ingredients.py

--- backend/recipe_ingredients.py
+++ backend/recipe_ingredients.py
@@ -1,2 +1,5 @@
 # Using the most common ingredients found in ingredient_counts.py, finds occurances of those ingredients in the recipies.
 # Author: Maxwell Romack
+
+prefix = 'recipes/'
+num_recipes = 100
