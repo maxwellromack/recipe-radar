f0d2b98eef3ec563f4f82c9c653c608be11665ec
Max <maxwellromack@gmail.com>
2023-05-30 13:50:27 -0400

    Update recipe_ingredients.py

--- backend/recipe_ingredients.py
+++ backend/recipe_ingredients.py
@@ -3,3 +3,26 @@
 
 prefix = 'recipes/'
 num_recipes = 100
+
+def get_ingredients(path):
+    ingredients_list = []
+    with open('common_ingredients.txt') as common:
+        for ingredient in common:
+            file = open(path, 'r')
+            if ingredient in file.read():
+                ingredients_list.append(1)
+            else:
+                ingredients_list.append(0)
+            file.close()
+
+    bin_string = ''
+    for digit in ingredients_list:
+        bin_string += str(digit)
+    return int(bin_string, 2)   # Converts the binary string to an int
+
+for id in range(num_recipes):
+    path = prefix + str(id) + '.txt'
+    with open(path, 'r') as check:
+        if 'METADATA:' not in check:    # Prevents adding the ingredients metadata to file if it already exists
+            with open(path, 'a') as file:
+                file.write('METADATA: ' + str(get_ingredients(path)) + '\n')
