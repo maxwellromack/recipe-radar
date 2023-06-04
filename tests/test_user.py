e15e8a48fa2d541d6f28e462badc779941e3a2f7
Max <maxwellromack@gmail.com>
2023-06-04 01:21:27 -0400

    added additional test

--- tests/test_user.py
+++ tests/test_user.py
@@ -30,3 +30,17 @@ def test_add_plural(client, auth):
 
     assert response.status_code == 201
     assert response.get_json() == {'message': 'Ingredient added'}
+
+def test_add_no_match(client, auth):
+    auth.register()
+    auth.login()
+
+    payload = { # i really hope that no recipe will ever use concrete as an ingredent
+        'input': 'concrete'
+    }
+
+    json_payload = json.dumps(payload)
+    response = client.post('/user/add', data = json_payload.encode('utf-8'), content_type = 'application/json')
+
+    assert response.status_code == 400
+    assert response.get_json() == {'message': 'No match found'}
