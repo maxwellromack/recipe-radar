462611175655361c57954baf23927b38af8facdc
Max <maxwellromack@gmail.com>
2023-06-04 00:38:34 -0400

    fixed file paths

--- backend/user.py
+++ backend/user.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, request, jsonify, g
 from backend.auth import login_required, get_db
 import numpy as np
-import sys, time
+import sys
 
 def get_max_length():
     max = 0
-    with open('ingredients_list.txt', 'r') as f:
+    with open('backend/ingredients_list.txt', 'r') as f:
         while line := f.readline():
             if max < len(line):
                 max = len(line)
@@ -14,7 +14,7 @@ def get_max_length():
 
 def get_num_ingredients():
     size = 0
-    with open('ingredients_list.txt', 'r') as file:
+    with open('backend/ingredients_list.txt', 'r') as file:
         for size, _ in enumerate(file):
             pass
     
@@ -25,7 +25,6 @@ bp = Blueprint('user', __name__, url_prefix = '/user')
 @bp.route('/add', methods = ['POST'])   # type: ignore
 @login_required
 def add():
-    start_time = time.time()
     data = request.get_json()
     input = data.get('input')
     user_id = g.user
@@ -46,7 +45,7 @@ def add():
     
     # one-hot encode the ingredients list
     ing_arr = np.zeros((size, length * 27), dtype = 'int')
-    with open('ingredients_list.txt', 'r') as f:
+    with open('backend/ingredients_list.txt', 'r') as f:
         for row in range(size):
             line = f.readline().rstrip()
             for i in range(len(line)):
@@ -61,8 +60,6 @@ def add():
     for row in range(size):
         neighbors[row] = np.linalg.norm(input_arr - ing_arr[row])
 
-    print("Ingredient add knn completed in " + str(time.time() - start_time) + " seconds.")
-    
     min = np.argmin(neighbors)
     if neighbors[min] == 0:   # perfect match found
         # TODO: add ingredient to db
@@ -70,8 +67,6 @@ def add():
     else:
         # TODO: similar ingredient suggestion
         pass
-    
-    
 
 if len(sys.argv) == 2:
     if sys.argv[1] == 'debug':
