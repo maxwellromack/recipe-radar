541886bfff874a9d0c6bc54a8ecafaaffa83d72d
Max <maxwellromack@gmail.com>
2023-06-04 12:12:09 -0400

    added input validation

--- backend/user.py
+++ backend/user.py
@@ -35,45 +35,54 @@ def add():
     input = input.replace('\'', '')
     input = input.replace('-', ' ')
 
-    # one-hot encode the input
-    input_arr = np.zeros(length * 27, dtype = 'int').T
-    for i in range(len(input)):
-        ins = ord(input[i])
-        if ins == 32:
-            input_arr[i * 27] = 1
-        else:
-            ins = (ins - 96) + (i * 27)
-            input_arr[ins] = 1
+    stripped = input.replace(' ', '')
+    if len(stripped) == 0:
+        error = 'Input is required'
+    elif not stripped.isalpha():
+        error = 'Input must contain only letters and spaces'
     
-    # one-hot encode the ingredients list
-    ing_arr = np.zeros((size, length * 27), dtype = 'int')
-    with open('backend/ingredients_list.txt', 'r') as f:
-        for row in range(size):
-            line = f.readline().rstrip()
-            for i in range(len(line)):
-                ins = ord(line[i])
-                if ins == 32:
-                    ing_arr[row, i * 27] = 1
-                else:
-                    ins = (ins - 96) + (i * 27)
-                    ing_arr[row, ins] = 1
+    if error is None:
+        # one-hot encode the input
+        input_arr = np.zeros(length * 27, dtype = 'int').T
+        for i in range(len(input)):
+            ins = ord(input[i])
+            if ins == 32:
+                input_arr[i * 27] = 1
+            else:
+                ins = (ins - 96) + (i * 27)
+                input_arr[ins] = 1
+        
+        # one-hot encode the ingredients list
+        ing_arr = np.zeros((size, length * 27), dtype = 'int')
+        with open('backend/ingredients_list.txt', 'r') as f:
+            for row in range(size):
+                line = f.readline().rstrip()
+                for i in range(len(line)):
+                    ins = ord(line[i])
+                    if ins == 32:
+                        ing_arr[row, i * 27] = 1
+                    else:
+                        ins = (ins - 96) + (i * 27)
+                        ing_arr[row, ins] = 1
 
-    neighbors = np.empty(size)
-    for row in range(size):
-        neighbors[row] = np.linalg.norm(input_arr - ing_arr[row])
-    
-    min = np.argmin(neighbors)
-    if neighbors[min] == 0: # perfect match found
-        # TODO: add ingredient to db
-        return jsonify({'message': 'Ingredient added'}), 201
-    elif neighbors[min] == 1 and input[-1] == 's':  # match found for singluar form of input
-        # TODO: add ingredient to db
-        return jsonify({'message': 'Singular ingredient added'}), 201
-    elif neighbors[min] < 2:    # possible spelling mistake, suggest similar ingredient to user
-        # TODO: similar ingredient suggestion
-        pass
+        neighbors = np.empty(size)
+        for row in range(size):
+            neighbors[row] = np.linalg.norm(input_arr - ing_arr[row])
+        
+        min = np.argmin(neighbors)
+        if neighbors[min] == 0: # perfect match found
+            # TODO: add ingredient to db
+            return jsonify({'message': 'Ingredient added'}), 201
+        elif neighbors[min] == 1 and input[-1] == 's':  # match found for singluar form of input
+            # TODO: add ingredient to db
+            return jsonify({'message': 'Singular ingredient added'}), 201
+        elif neighbors[min] < 2:    # possible spelling mistake, suggest similar ingredient to user
+            # TODO: similar ingredient suggestion
+            pass
+        else:
+            return jsonify({'message': 'No match found'}), 400
     else:
-        return jsonify({'message': 'No match found'}), 400
+        return jsonify({'error': error}), 400
 
 if len(sys.argv) == 2:
     if sys.argv[1] == 'debug':
