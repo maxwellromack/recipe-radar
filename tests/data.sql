ba1c7a6ea0193fa2b4f6903bc4ae9121505af646
Max <maxwellromack@gmail.com>
2023-06-04 11:43:54 -0400

    Update data.sql

--- tests/data.sql
+++ tests/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user (username, password)
 VALUES
-    ('littleBobbyTables', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f'),
-    ('test', 'pbkdf2:sha256:600000$xJMcyc3vmVuQUKfI$ae33b75b3e1d4cbea7a10cb163e5ac24816bb9db252c907c49a5731654bb57c4');
+    ('littleBobbyTables', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', '0'),
+    ('test', 'pbkdf2:sha256:600000$xJMcyc3vmVuQUKfI$ae33b75b3e1d4cbea7a10cb163e5ac24816bb9db252c907c49a5731654bb57c4', '0');
