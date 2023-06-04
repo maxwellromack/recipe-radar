4c3879de4a47e254acdb96e3c81bded0e8308af8
Max <maxwellromack@gmail.com>
2023-06-04 12:25:43 -0400

    added test for database checking

--- tests/test_user.py
+++ tests/test_user.py
@@ -81,3 +81,32 @@ def test_add_validation(client, auth, input, message):
 
     assert response.status_code == 400
     assert message in response.get_json()['error']
+
+def test_add_db(client, auth, app):
+    auth.regster()
+    auth.login()
+
+    payload = {
+        'input': 'salt'
+    }
+
+    json_payload = json.dumps(payload)
+    client.post('user/add', data = json_payload.encode('utf-8'), content_type = 'application/json')
+
+    with app.app_context():
+        db = get_db()
+        user_ingredients = (db.execute(
+            'SELECT ingredients FROM user WHERE id = 3'
+        )).fetchone()[0]
+
+    size = 0
+    with open('backend/ingredients_list.txt', 'r') as file:
+        for size, _ in enumerate(file):
+            pass
+
+    bin_str = ''
+    for i in range(size):
+        bin_str += '0'
+    bin_str[0] = 1  # type: ignore
+    
+    assert user_ingredients == bin_str
