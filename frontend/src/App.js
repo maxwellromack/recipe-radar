bef4439c3725bdb5a1fadb8de54e60b24e826f0c
SaurabhK24 <Skhanal247@gmail.com>
2023-06-12 21:37:17 -0400

    Error Fixes for Landing.js, App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Hero from './components/Hero';
-import Login from './components/Login';
-import Register from './components/Register';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         {/* Define your routes here */}
-        <Route path="/" element={<Hero />} /> {/* Render Hero component as main page */}
+        <Route path="/" element={<Landing />} /> {/* Render Hero component as main page */}
         <Route path="/login" element={<Login />} /> {/* Render Login component when URL is /login */}
         <Route path="/register" element={<Register />} /> {/* Render Register component when URL is /register */}
       </Routes>
