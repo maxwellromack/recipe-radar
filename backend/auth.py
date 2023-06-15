f5f9d27981742c3a2b4f85657b86406ce061be4f
Max <78194977+maxwellromack@users.noreply.github.com>
2023-06-15 12:20:18 -0400

    API returns status code when there is an error

--- backend/auth.py
+++ backend/auth.py
@@ -58,8 +58,8 @@ def register():
             except db.IntegrityError:   # username already exists
                 error = f"User {username} is already registered."
             else:
-                res = make_response()
-                #res = corsify_response(jsonify({'message': 'Registration success'}))
+                #res = make_response()
+                res = corsify_response(make_response(jsonify({'message': 'Registration success'})))
                 res.status_code = 201
                 return res
 
