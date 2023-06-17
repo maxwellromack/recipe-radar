d705f734712006283aecf51907b865eaeb04bce9
Max <maxwellromack@gmail.com>
2023-06-17 11:37:44 -0400

    fixed bad fetch request formatting

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -13,24 +13,24 @@ function Register() {
         setPassword(event.target.value);
     }
 
-    async function handleRegister() {
-            const payload = {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
+    const handleRegister = async event => {
+        event.preventDefault();
+        try {
+            await fetch('http://localhost:5000/auth/register', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify({
                     username: user,
                     password: pass
                 })
-            };
-
-            await fetch('http://127.0.0.1:5000/auth/register', payload)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data)
-                })
-                .catch(error => console.error(error));
+            })
+                .then(Response => console.log(Response))
+                .catch(err => console.log(err));
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
